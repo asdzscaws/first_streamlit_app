3b158173d7ce8120951dadef08b54431f0493281
asdzscaws <126447983+asdzscaws@users.noreply.github.com>
2023-02-28 13:59:12 +0800

    Update streamlit_app.py
    remove json text

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,6 @@ streamlit.dataframe(fruits_to_show)
 # New Section to display fruitvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
-
 # Take the json version of the reponse and normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Output it the screen as a table
